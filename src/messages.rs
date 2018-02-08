75f62a62adb3726f5771c0988f4c9130f2606fbf
Benjamin Landers <benjaminrlanders@gmail.com>
2018-02-07 21:10:20 -0800

    Make handle_message work + reorder to make more logical

--- src/messages.rs
+++ src/messages.rs
@@ -1,3 +1,11 @@
+// Structs for low level parsing
+#[derive(Debug)]
+pub struct Move(Point, u8, u32);
+
+#[derive(Debug)]
+pub struct Point(u32, u32);
+
+// Full Messages
 #[derive(Debug)]
 pub enum GameMessage {
     Update(u8, u8, Vec<Point>, Vec<u32>),
@@ -6,9 +14,13 @@ pub enum GameMessage {
 }
 
 enum MetaMessage {
-    other,
+    Error(String, MessageType),
+    Chat(String),
+    EnterRoom(String),
 }
 
+// First byte datatypes
+#[derive(Debug)]
 enum MessageType {
     Login,
     EnterRoom,
@@ -28,26 +40,7 @@ enum MessageType {
     Internal,
 }
 
-fn parse_message_type(meta: MetaData, id: u8) -> Option<MessageType> {
-    if meta.meta {
-        Some(match id {
-            1 => Login,
-            2 => EnterRoom,
-            3 => Ping,
-            4 => Info,
-            _ => return None,
-        })
-    } else {
-        Some(match id {
-            1 => Updatecell,
-            2 => Move,
-            3 => Status,
-            4 => Replay,
-            _ => return None,
-        })
-    }
-}
-
+#[derive(Clone, Debug)]
 struct MetaData {
     info: bool,
     error: bool,
@@ -55,80 +48,37 @@ struct MetaData {
     from_server: bool,
 }
 
-pub struct Error {
-    text: &'static str,
-    code: MessageType,
-}
-fn parse_GameMessage(typ: MessageType, data: Vec<u8>) -> Option<GameMessage> {
-    match typ {
-        MessageType::UpdateCell => parse_update(data),
-        MessageType::Move => parse_move(data),
-        MessageType::Replay => parse_replay(data),
-        _ => None,
+// Functions to marshal in between a byte and the first byte metadata
+fn parse_message_type(meta: MetaData, id: u8) -> Option<MessageType> {
+    if meta.meta {
+        Some(match id {
+            1 => MessageType::Login,
+            2 => MessageType::EnterRoom,
+            3 => MessageType::Ping,
+            4 => MessageType::Info,
+            _ => return None,
+        })
+    } else {
+        Some(match id {
+            1 => MessageType::UpdateCell,
+            2 => MessageType::Move,
+            3 => MessageType::Status,
+            4 => MessageType::Replay,
+            _ => return None,
+        })
     }
 }
 
-pub fn parse_update(data: Vec<u8>) -> Option<GameMessage> {
-    let mut it = data.into_iter();
-    let player = it.next()?;
-    let width = it.next()?;
-    let mut points = Vec::new();
-    let mut values = Vec::new();
-    loop {
-        let first = match it.next() {
-            Some(val) => val,
-            None => break,
-        };
-        let loc = parse_point_private(first, &mut it, width as u32)?;
-        let value = parse_unbounded(&mut it)?;
-        points.push(loc);
-        values.push(value);
+fn meta_from_byte(first: u8) -> MetaData {
+    MetaData {
+        meta: first & 0x80 == 1,
+        error: first & 0x40 == 1,
+        info: first & 0x20 == 1,
+        from_server: first & 0x10 == 1,
     }
-    Some(GameMessage::Update(player, width, points, values))
 }
 
-pub fn parse_move(data: Vec<u8>) -> Option<GameMessage> {
-    let mut it = data.into_iter();
-    let width = it.next()? as u32;
-    Some(GameMessage::MoveMessage(
-        width,
-        parse_move_private(&mut it, width)?,
-    ))
-}
-
-#[derive(Debug)]
-pub struct Move(Point, u8, u32);
-
-fn parse_move_private<I>(it: &mut I, width: u32) -> Option<Move>
-where
-    I: Iterator<Item = u8>,
-{
-    let point = parse_point(it, width)?;
-    let direction = it.next()?;
-    let units = parse_unbounded(it)?;
-    Some(Move(point, direction, units))
-}
-
-fn parse_replay(data: Vec<u8>) -> Option<GameMessage> {
-    let mut it = data.into_iter();
-    let player = it.next()?;
-    let width = it.next()? as u32;
-    let mut moves = Vec::new();
-    let mut skips = Vec::new();
-
-    loop {
-        let first = match it.next() {
-            Some(val) => val,
-            None => break,
-        };
-        let skip = parse_unbounded_private(first, &mut it)?;
-        let mov = parse_move_private(&mut it, width)?;
-        skips.push(skip);
-        moves.push(mov);
-    }
-
-    Some(GameMessage::Replay(player, width, moves, skips))
-}
+// Low level parsing primitives
 pub fn parse_unbounded<I>(it: &mut I) -> Option<u32>
 where
     I: Iterator<Item = u8>,
@@ -147,9 +97,6 @@ where
     })
 }
 
-#[derive(Debug)]
-pub struct Point(u32, u32);
-
 pub fn parse_point<I>(it: &mut I, width: u32) -> Option<Point>
 where
     I: Iterator<Item = u8>,
@@ -165,8 +112,96 @@ where
     Some(Point(num % width, num / width))
 }
 
+
+fn parse_move_private<I>(it: &mut I, width: u32) -> Option<Move>
+where
+    I: Iterator<Item = u8>,
+{
+    let point = parse_point(it, width)?;
+    let direction = it.next()?;
+    let units = parse_unbounded(it)?;
+    Some(Move(point, direction, units))
+}
+
+// Parsing for full GameMessages of a particular type
+pub fn parse_update<I>(it: &mut I) -> Option<GameMessage>
+where
+    I: Iterator<Item = u8>,
+{
+    let player = it.next()?;
+    let width = it.next()?;
+    let mut points = Vec::new();
+    let mut values = Vec::new();
+    loop {
+        let first = match it.next() {
+            Some(val) => val,
+            None => break,
+        };
+        let loc = parse_point_private(first, it, width as u32)?;
+        let value = parse_unbounded(it)?;
+        points.push(loc);
+        values.push(value);
+    }
+    Some(GameMessage::Update(player, width, points, values))
+}
+
+pub fn parse_move<I>(it: &mut I) -> Option<GameMessage>
+where
+    I: Iterator<Item = u8>,
+{
+    let width = it.next()? as u32;
+    Some(GameMessage::MoveMessage(
+        width,
+        parse_move_private(it, width)?,
+    ))
+}
+
+
+fn parse_replay<I>(it: &mut I) -> Option<GameMessage>
+where
+    I: Iterator<Item = u8>,
+{
+    let player = it.next()?;
+    let width = it.next()? as u32;
+    let mut moves = Vec::new();
+    let mut skips = Vec::new();
+
+    loop {
+        let first = match it.next() {
+            Some(val) => val,
+            None => break,
+        };
+        let skip = parse_unbounded_private(first, it)?;
+        let mov = parse_move_private(it, width)?;
+        skips.push(skip);
+        moves.push(mov);
+    }
+
+    Some(GameMessage::Replay(player, width, moves, skips))
+}
+
+// Highest level GameMessage parser
+fn parse_GameMessage<I>(typ: MessageType, data: &mut I) -> Option<GameMessage>
+where
+    I: Iterator<Item = u8>,
+{
+    match typ {
+        MessageType::UpdateCell => parse_update(data),
+        MessageType::Move => parse_move(data),
+        MessageType::Replay => parse_replay(data),
+        _ => None,
+    }
+}
+
+
+pub struct Error {
+    text: &'static str,
+    code: MessageType,
+}
+
 pub fn handle_message(data: Vec<u8>) -> Result<(), Error> {
-    let first = 0x4F;
+    let mut it = data.into_iter();
+    let first = it.next().unwrap_or(0);
     let meta = meta_from_byte(first);
     if meta.from_server {
         return Err(Error {
@@ -175,21 +210,25 @@ pub fn handle_message(data: Vec<u8>) -> Result<(), Error> {
         });
     }
 
-    if meta.meta {
-        return Err(Error {
-            text: "Expected a GameMessage, but has the Meta field set",
-            code: MessageType::MalformedMessage,
-        });
+    let msg_type = match parse_message_type(meta.clone(), first) {
+        Some(msg) => msg,
+        None => {
+            return Err(Error {
+                text: "Malformed first byte: MessageType unknown",
+                code: MessageType::MalformedMessage,
+            })
+        }
+    };
+    println!("{:?}, {:?} ", msg_type, meta);
+    if !meta.meta {
+        let game_message = match parse_GameMessage(msg_type, &mut it){
+		Some(msg)=>msg,
+		None => return Err(Error{text: "Malformed GameMessage data: Failed to parse the data according to the message type",
+					 code: MessageType::MalformedMessage}),
+		
+		};
+        println!("{:?}", game_message);
     }
 
     Ok(())
-}
-
-fn meta_from_byte(first: u8) -> MetaData {
-    MetaData {
-        meta: first & 0x80 == 1,
-        error: first & 0x40 == 1,
-        info: first & 0x20 == 1,
-        from_server: first & 0x10 == 1,
-    }
 }
